d89aa48780a1958ab0cf8dfce93bbc056b5fdd86
JethroMuluba <jethro.muluba@gmail.com>
2024-01-23 20:36:07 +0100

    Style: Install Tailwind and stylised Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useForm } from 'react-hook-form';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      const [formData,setFormData] = useState(
+          {
+              name:"",
+              phone:"",
+              email:"",
+              age:"18",
+              gender:""
+          }
+      );
+
+      const {register, handleSubmit, formState:{errors}} = useForm({defaultValues:formData});
+
+      const onSubmit = (data) => {
+            console.log(data);
+            alert(`${data.name} a ete enregistre`);
+      }
+
+      return (
+
+      <form onSubmit = {handleSubmit (onSubmit)} className='flex flex-col justify-center gap-[20px] w-[300px] p-[20px] border border-gray rounded-[10px] shadow-lg '>
+
+          <div className='flex flex-col justify-start'>
+              <label>Nom</label>
+              <input placeholder='Taper votre nom ici' type="text" name='name' {...register("name", {required:true})} className='border rounded-full border-gray' />
+              {errors.name && <span className="border-2 border-rose-500">  Saisis de nom obligatoire</span>}
+          </div>
+
+          <div className='flex flex-col justify-start'>
+              <label>Telephone</label>
+              <input name='Phone' type="text" {...register("Phone", { pattern:/^[0-9]{10}$/i}, {required:true})} className='border rounded-full border-gray'/>
+              {errors.name && <span style={{color: 'red'}}>  Saisis de numéro obligatoire</span>}
+
+          </div>
+
+          <div className='flex flex-col justify-start'>
+              <label>Email</label>
+              <input name='email' type="email" {...register("email")} className='border rounded-full border-gray' />
+          </div>
+
+          <div className='flex flex-col justify-start'>
+              <label>Age</label>
+              <input name='age' type="number" {...register("age")} className='border rounded-full border-gray' />
+          </div>
+
+          <div className='flex flex-col justify-start'>
+              <label>Genre</label>
+                <select {...register("age")}>
+                  <option value="female">Femme</option>
+                  <option value="male">Homme</option>
+                  <option value="other">Autre</option>
+                </select>
+          </div>
+
+          <button type='submit'>Enregister</button>
+      </form>
   )
 }
-
 export default App
