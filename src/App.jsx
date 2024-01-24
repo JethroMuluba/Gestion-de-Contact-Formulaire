6de034492e3ad8b3f227d85b8f2b0302664d893b
JethroMuluba <jethro.muluba@gmail.com>
2024-01-24 10:05:05 +0100

    Style: Stylised button

--- src/App.jsx
+++ src/App.jsx
@@ -5,61 +5,71 @@ import { useForm } from 'react-hook-form';
 
 function App() {
 
-      const [formData,setFormData] = useState(
-          {
-              name:"",
-              phone:"",
-              email:"",
-              age:"18",
-              gender:""
-          }
-      );
+        const [formData,setFormData] = useState(
+            {
+                name:"",
+                phone:"",
+                email:"",
+                age:"18",
+                gender:""
+            }
+        );
 
-      const {register, handleSubmit, formState:{errors}} = useForm({defaultValues:formData});
+        const {register, handleSubmit, formState:{errors}} = useForm({defaultValues:formData});
 
-      const onSubmit = (data) => {
+        const onSubmit = (data) => {
             console.log(data);
             alert(`${data.name} a ete enregistre`);
-      }
+        }
 
-      return (
+        return (
 
-      <form onSubmit = {handleSubmit (onSubmit)} className='flex flex-col justify-center gap-[20px] w-[300px] p-[20px] border border-gray rounded-[10px] shadow-lg '>
+    <div className='flex flex-col gap-[20px] '>
 
-          <div className='flex flex-col justify-start'>
-              <label>Nom</label>
-              <input placeholder='Taper votre nom ici' type="text" name='name' {...register("name", {required:true})} className='border rounded-full border-gray' />
-              {errors.name && <span className="border-2 border-rose-500">  Saisis de nom obligatoire</span>}
-          </div>
+        <div className='flex flex-col  gap-[4px]'>
+            <h1 className='text-2xl font-bold'>Gestion de Contact </h1>
+            <h2 className='text-sm'>By Jethro Code</h2>
+        </div>
 
-          <div className='flex flex-col justify-start'>
-              <label>Telephone</label>
-              <input name='Phone' type="text" {...register("Phone", { pattern:/^[0-9]{10}$/i}, {required:true})} className='border rounded-full border-gray'/>
-              {errors.name && <span style={{color: 'red'}}>  Saisis de numéro obligatoire</span>}
+        <form onSubmit = {handleSubmit (onSubmit)} className='flex flex-col justify-center gap-[20px] w-[300px] p-[20px] border border-gray rounded-[10px] shadow-lg '>
 
-          </div>
+            <div className='flex flex-col items-start '>
+                <label className="text-lg font-bold ">Nom</label>
+                <input placeholder='Taper votre nom ici' type="text" name='name' {...register("name", {required:true})} className='bg-none border rounded-[10px] border-[#1a1a1a] text-lg py-[5px] pl-[20px] hover:border-[#3d3d3d] ' />
+                {errors.name && <span className="border-2 border-rose-500">  Saisis de nom obligatoire</span>}
+            </div>
 
-          <div className='flex flex-col justify-start'>
-              <label>Email</label>
-              <input name='email' type="email" {...register("email")} className='border rounded-full border-gray' />
-          </div>
+            <div className='flex flex-col items-start'>
+                <label className="text-lg font-bold ">Téléphone</label>
+                <input  placeholder='Taper votre numéro ici' name='Phone' type="text" {...register("Phone", { pattern:/^[0-9]{10}$/i}, {required:true})} className='bg-none border rounded-[10px] border-[#1a1a1a] text-lg py-[5px] pl-[20px] focus:border-[#3d3d3d]'/>
+                {errors.name && <span style={{color: 'red'}}>  Saisis de numéro obligatoire</span>}
+            </div>
 
-          <div className='flex flex-col justify-start'>
-              <label>Age</label>
-              <input name='age' type="number" {...register("age")} className='border rounded-full border-gray' />
-          </div>
+            <div className='flex flex-col items-start'>
+                <label className="text-lg font-bold " >Email</label>
+                <input  placeholder='Taper votre email ici' name='email' type="email" {...register("email")} className='bg-none border rounded-[10px] border-[#1a1a1a] text-lg py-[5px] pl-[20px] focus:border-[#3d3d3d]' />
+            </div>
 
-          <div className='flex flex-col justify-start'>
-              <label>Genre</label>
-                <select {...register("age")}>
-                  <option value="female">Femme</option>
-                  <option value="male">Homme</option>
-                  <option value="other">Autre</option>
+            <div className='flex flex-col items-start'>
+                <label className="text-lg font-bold ">Age</label>
+                <input name='age' type="number" {...register("age")} className='bg-none border rounded-[10px] border-[#1a1a1a] text-lg py-[5px] pl-[20px] focus:border-[#3d3d3d]' />
+            </div>
+
+            <div className='flex flex-col items-start'>
+                <label className="text-lg font-bold ">Genre</label>
+                <select {...register("age")} className='bg-none border rounded-[10px] border-[#1a1a1a] text-lg py-[5px] pl-[20px] focus:border-[#3d3d3d]'>
+                    <option value="female">Femme</option>
+                    <option value="male">Homme</option>
+                    <option value="other">Autre</option>
                 </select>
-          </div>
+            </div>
 
-          <button type='submit'>Enregister</button>
-      </form>
-  )
+            <button type='submit' className='bg-[#213547] hover:bg-cyan-600 hover:border-slate-400  text-white'>Enregister</button>
+
+        </form>
+
+    </div>
+
+    )
 }
 export default App
